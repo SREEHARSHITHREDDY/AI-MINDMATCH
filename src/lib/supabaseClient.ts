5303e4a53a999131370f0d4d8fa5d5d77b206065
SREEHARSHITHREDDY <reddyharshith20@gmail.com>
2025-09-21 17:37:06 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-// src/lib/supabaseClient.ts
+// src/supabase/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!; // <-- corrected key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
